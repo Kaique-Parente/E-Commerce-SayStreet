8040ecb2a67f97d59cdb56f90189d58078e00681
RodrigoPereir4 <digolima167@gmail.com>
2025-02-23 18:27:26 -0300

    [CREATE] Classe DTO de usuário

---
+++ backend/src/main/java/com/saystreet/backend/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.saystreet.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserDto {
+    
+    private String email;
+
+    private String password;
+
+    private boolean status;
+
+}
