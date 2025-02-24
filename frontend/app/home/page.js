bd4bd40c6115d4f39308dde064385fe58b165a51
Kaique-Parente <kaique.parente.game@outlook.com>
2025-02-23 22:32:20 -0300

    [FRONTEND ADD] Tela Principal.

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,6 +1,42 @@
+'use client'
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    border: 1px solid white;
+
+    gap: 30px;
+`
+
+const LinksContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+
+    a{
+        font-size: 30px;
+        text-decoration: underline;
+    }
+`
 
 export default function Home(){
     return (
-        <h1>Bem-vindo</h1>
+        <Container>
+            <h1>Principal</h1>
+
+            <LinksContainer>
+                <a href="./">Listar Produto</a>
+                <a href="./">Listar Usuário</a>
+                <a href="./">Listar Pedidos</a>
+            </LinksContainer>
+        </Container>
     );
 }
