8b87b04fdf83461a767f173e4a025fcd19308cb4
RodrigoPereir4 <digolima167@gmail.com>
2025-03-04 01:22:10 -0300

    [BACKEND CREATE] Classe DTO do Produto

---
+++ backend/src/main/java/com/saystreet/backend/dto/ProdutoDto.java
@@ -0,0 +1,19 @@
+package com.saystreet.backend.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProdutoDto {
+    
+    private String produto_nome;
+    
+    private double produto_avaliacao;
+
+    private Integer produto_qtd;
+
+    private boolean status;
+}
