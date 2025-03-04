c206de63c773d4e8ed4811aab37ed17212c93f4c
RodrigoPereir4 <digolima167@gmail.com>
2025-03-04 01:23:08 -0300

    [BACKEND CREATE] Classe repository do Produto.

---
+++ backend/src/main/java/com/saystreet/backend/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.saystreet.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.saystreet.backend.models.ProdutoModel;
+
+public interface ProdutoRepository extends JpaRepository <ProdutoModel, Long> {
+    
+}
