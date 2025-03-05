7f1bff6f9954587e04de2325548be09aa8a3c892
RodrigoPereir4 <digolima167@gmail.com>
2025-03-05 00:34:17 -0300

    [BACKEND UPDATE] Método para buscar produto por ID + Refatoração do código para salvar produto, utilizando agora o DTO.

--- backend/src/main/java/com/saystreet/backend/service/ProdutoService.java
+++ backend/src/main/java/com/saystreet/backend/service/ProdutoService.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.saystreet.backend.dto.ProdutoDto;
 import com.saystreet.backend.models.ProdutoModel;
 import com.saystreet.backend.repository.ProdutoRepository;
 
@@ -24,7 +25,7 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public String create(ProdutoModel produto, MultipartFile arquivo) {
+    public String create(ProdutoDto produtoDto, MultipartFile arquivo) {
         try {
             if (arquivo.isEmpty()) {
                 return "Arquivo não pode estar vazio.";
@@ -41,15 +42,31 @@ public class ProdutoService {
             Path caminhoArquivo = Paths.get(UPLOAD_DIR, nomeArquivo);
             Files.write(caminhoArquivo, arquivo.getBytes());
 
-            // Atualizar o nome da imagem no produto
-            produto.setNome_imagem(nomeArquivo);
 
-            produtoRepository.save(produto);
+            ProdutoModel produtoSalvo = new ProdutoModel(produtoDto.getProduto_name(), 
+            produtoDto.getProduto_avaliacao(), produtoDto.getProduto_qtd(), nomeArquivo);
+            produtoRepository.save(produtoSalvo);
 
             return "Produto salvo com sucesso!";
 
         } catch (IOException e) {
             return "Erro ao salvar arquivo: " + e.getMessage();
+        }
+    }
+
+    public ProdutoModel buscarProdutoPorId(Long id) {
+        Optional<ProdutoModel> produtoOpt = produtoRepository.findById(id);
+        
+        if (produtoOpt.isPresent()) {
+            ProdutoModel produto = produtoOpt.get();
+    
+            // Definir o caminho completo da imagem
+            String caminhoImagem = UPLOAD_DIR + "/" + produto.getNome_imagem();
+            produto.setNome_imagem(caminhoImagem);
+    
+            return produto;
+        } else {
+            throw new NoSuchElementException("Produto não encontrado!");
         }
     }
 }
