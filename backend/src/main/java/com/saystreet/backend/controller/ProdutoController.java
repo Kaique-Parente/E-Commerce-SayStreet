fcdbe8c80639e75a046cb6605ab36ad768d859fd
RodrigoPereir4 <digolima167@gmail.com>
2025-03-08 18:03:06 -0300

    [BACKEND UPDATE] Refatoração dos Endpoint

--- backend/src/main/java/com/saystreet/backend/controller/ProdutoController.java
+++ backend/src/main/java/com/saystreet/backend/controller/ProdutoController.java
@@ -13,11 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saystreet.backend.dto.ProdutoDto;
 import com.saystreet.backend.models.ProdutoModel;
 import com.saystreet.backend.service.ProdutoService;
@@ -31,16 +27,10 @@ public class ProdutoController {
     ProdutoService produtoService;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> salvar(
-            @RequestParam("produto") String produtoJson,
-            @RequestParam("file") MultipartFile arquivo) throws JsonMappingException, JsonProcessingException {
-        
-        // Convertendo o JSON para ProdutoDTO
-        ProdutoDto produto = new ObjectMapper().readValue(produtoJson, ProdutoDto.class);
-
-        return ResponseEntity.ok(produtoService.create(produto, arquivo));
+    public ResponseEntity<?> salvar(@RequestBody ProdutoDto produto){
+        return ResponseEntity.ok(produtoService.create(produto));
     }
-
+    
      @GetMapping("/buscar/{id}")
     public ResponseEntity<ProdutoModel> buscarProduto(@PathVariable Long id) {
         ProdutoModel produto = produtoService.buscarProdutoPorId(id);
@@ -49,13 +39,9 @@ public class ProdutoController {
 
     @PutMapping("/editar/{id}")
     public ResponseEntity<String> editarProduto(@PathVariable Long id,
-            @RequestParam("produto") String produtoJson,
-            @RequestParam("file") MultipartFile arquivo) throws JsonProcessingException {
+            @RequestBody ProdutoDto produtoDto) {
 
-        // Convertendo o JSON para ProdutoDTO        
-        ProdutoDto produtoDto = new ObjectMapper().readValue(produtoJson, ProdutoDto.class);
-
-        return ResponseEntity.ok(produtoService.editarProduto(id, produtoDto, arquivo));
+        return ResponseEntity.ok(produtoService.editarProduto(id, produtoDto));
     }
     
     @PutMapping("/alterar-status/{id}")
