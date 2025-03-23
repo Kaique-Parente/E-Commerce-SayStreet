bc9320c0ede9600dfe1b11a6863cd7a90c57682e
Kaique-Parente <kaique.parente.game@outlook.com>
2025-03-23 17:37:23 -0300

    [FRONTEND ADD] Lógica de puxar as informações do produto clickado na landing page para a página de detalhes

--- frontend/app/client/[slug]/page.js
+++ frontend/app/client/[slug]/page.js
@@ -1,6 +1,8 @@
 'use client'
 
 import NavBar from "@/components/ClientComponents/NavBar";
+import { encontrarProdutoId } from "@/services/ProdutoService";
+import { assignRef } from "@coreui/react/dist/esm/hooks/useForkedRef";
 import { useSearchParams } from "next/navigation";
 import { use, useEffect, useState } from 'react';
 import styled from "styled-components";
@@ -17,6 +19,24 @@ const Container = styled.div`
 export default function ProdutoDetalhes({ params }) {
 
     const [id, setId] = useState(null);
+    const [produto, setProduto] = useState({});
+    const [erro, setErro] = useState([]);
+
+    const handleProcurarProduto = async (e) => {
+        console.log(id);
+        try{
+            const response = await encontrarProdutoId(id);
+            if(response!=null){
+               setProduto(response);
+            }else {
+                alert("Produto não encontrado");
+            }
+        }catch (error) {
+            console.log(error);
+            setErro("Erro de comunicação com o servidor!");
+        }
+    }
+
 
     // Acessando o 'slug' vindo da URL
     const resolvedParams = use(params); // Usando React.use() para resolver a Promise de params
@@ -30,13 +50,33 @@ export default function ProdutoDetalhes({ params }) {
         }
     }, [slug]);
 
+    useEffect(() => {
+        if(id){
+            handleProcurarProduto();
+        }
+    }, [id])
+
+    useEffect(() => {
+        console.log(produto);
+    }, [produto])
+
+   
     return (
         <>
             <NavBar />
             <Container>
                 <div>
+                    <div>
+                        
+                    </div>
+
+                    <div>
+
+                    </div>
+
                     <h1 style={{ marginTop: 100 }}>Detalhes do Produto</h1>
                     <p>Produto: {slug}</p>
+                    <p>Nome do Produto: {produto.produtoNome}</p>
                     <p>ID: {id}</p>
                 </div>
             </Container>
