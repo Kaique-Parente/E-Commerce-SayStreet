9416974ec215917c55b3fe253f3f3187666119e6
Kaique-Parente <kaique.parente.game@outlook.com>
2025-03-27 14:05:29 -0300

    [FRONTEND ADD] Radius Button das Transportadoras, tamanho adaptavel ao primeiro container.

--- frontend/app/carrinho/page.js
+++ frontend/app/carrinho/page.js
@@ -1,35 +1,123 @@
 "use client"
 
+import BotaoPersonalizado from "@/components/ClientComponents/BotaoPersonalizado";
+import Footer from "@/components/ClientComponents/Footer";
+import NavBar from "@/components/ClientComponents/NavBar";
+import TransportadorasGroup from "@/components/ClientComponents/TransportadorasGroup";
 import { useCarrinho } from "@/context/CarrinhoContext";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    padding-top: 80px;
+`
 
 export default function Carrinho() {
     const { carrinho } = useCarrinho();
+    const [transportadora, setTransportadora] = useState("");
 
     return (
         <>
-            <h1>Meu Carrinho</h1>
-            {carrinho.map((item) => {
-                console.log(item); // Agora, o console.log está dentro da função de callback do map, antes do return
-                return (
-                    <div key={`${item.produtoId}-${item.produtoTamanho}`}>
-                        <p>ID: {item.produtoId}</p>
-                        <p>Nome: {item.produtoNome}</p>
-                        <p>Preço: {item.produtoPreco}</p>
-                        <p>Tamanho: {item.produtoTamanho}</p>
-
-                        {/* Verificar se imagem é um objeto e renderizar a URL */}
-                        {item.imagem && item.imagem.url ? (
-                            <Image width={372} height={390} src={item.imagem.url} alt={item.produtoNome} />
-                        ) : (
-                            <p>Imagem não disponível</p>
-                        )}
-
-                        <p>Quantidade: {item.quantidade}</p>
+            <NavBar />
+            <Container>
+                <div>
+                    <div>
+                        <div>1</div>
+                        <span>Carrinho</span>
                     </div>
-                );
-            })}
+                    <div>
+                        <div>2</div>
+                        <span>Identificação</span>
+                    </div>
+                    <div>
+                        <div>3</div>
+                        <span>Pagamento</span>
+                    </div>
+                </div>
+                {carrinho.map((item) => {
+                    console.log(item); // Agora, o console.log está dentro da função de callback do map, antes do return
+                    return (
+                        <>
+                            <div key={`${item.produtoId}-${item.produtoTamanho}`}>
 
+                                <div>
+                                    {item.imagem && item.imagem.url ? (
+                                        <Image width={372} height={390} src={item.imagem.url} alt={item.produtoNome} style={{ objectFit: "contain" }} />
+                                    ) : (
+                                        <p>Imagem não disponível</p>
+                                    )}
+                                </div>
+
+                                <div>
+                                    <Link href={""}>Nome: {item.produtoNome}</Link>
+                                    <p>Tamanho: {item.produtoTamanho}</p>
+                                </div>
+
+                                <div>
+                                    <h3>Quantidade:</h3>
+                                    <div>
+                                        <button>
+                                            <Image width={14} height={14} src={"/web/seta-esquerda.svg"} alt="Ícone de seta para a esquerda" />
+                                        </button>
+                                        <p>{item.quantidade}</p>
+                                        <button>
+                                            <Image width={14} height={14} src={"/web/seta-direita.svg"} alt="Ícone de seta para a direita" />
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button>
+                                            <Image width={14} height={14} src={"/backoffice/lixo.svg"} alt="Ícone de Lixo" />
+                                            <span>Remover</span>
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <h3>Valor total</h3>
+                                    <p>{item.produtoPreco * item.quantidade}</p>
+                                </div>
+
+                            </div>
+                        </>
+                    );
+                })}
+
+                <div>
+                    <div>
+                        <div>
+                            <h2>Resumo do Pedido</h2>
+                            <p>Valor dos produtos: <span>R$ {0.00}</span></p>
+                            <p>Frete: <span>R$ {0.00}</span></p>
+                            <div>
+                                <h3>Valor Total: <span>R$ {0.00}</span></h3>
+                                <p>(em até <span>10x</span> de <span>R$ 585,88</span> sem juros)</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div style={{width: "500px"}}>
+                            <h2>Calcular Frete</h2>
+                            <div>
+                                <input type="number" placeholder="CEP:" />
+                                <button>OK</button>
+                            </div>
+                            <TransportadorasGroup transportadora={transportadora} setTransportadora={setTransportadora}/>
+                        </div>
+                    </div>
+
+                    <BotaoPersonalizado width={300} height={100}>Finalizar a Compra</BotaoPersonalizado>
+                </div>
+
+                <Footer />
+            </Container>
         </>
     );
 }
---
+++ frontend/components/ClientComponents/TransportadorasGroup.js
@@ -0,0 +1,101 @@
+"use client"
+
+import { FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { useState, useEffect } from "react";
+
+export default function TransportadorasGroup({ transportadora, setTransportadora }) {
+    const [dataEntrega, setDataEntrega] = useState("");
+
+    // Função para calcular a data de entrega (data atual + X dias)
+    useEffect(() => {
+        // Se não houver transportadora selecionada, não faz nada
+        if (!transportadora) return;
+
+        const dataAtual = new Date();
+
+        // Definir os prazos de entrega de acordo com a transportadora selecionada
+        switch (transportadora) {
+            case "transportadora1":
+                dataAtual.setDate(dataAtual.getDate() + 5); // 5 dias
+                break;
+            case "transportadora2":
+                dataAtual.setDate(dataAtual.getDate() + 3); // 3 dias
+                break;
+            case "transportadora3":
+                dataAtual.setDate(dataAtual.getDate() + 2); // 2 dias
+                break;
+            default:
+                break;
+        }
+
+        const dataFormatada = dataAtual.toLocaleDateString(); // Formato padrão de data
+        setDataEntrega(dataFormatada);
+    }, [transportadora]);
+
+    const handleChange = (event) => {
+        // Atualiza o estado da transportadora com o valor selecionado
+        setTransportadora(event.target.value);
+    };
+
+    return (
+        <FormControl style={{ width: "100%" }}>
+            <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={transportadora}
+                onChange={handleChange} // A função que atualiza o estado
+            >
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <FormControlLabel
+                        value="transportadora1"
+                        control={<Radio />}
+                        label={
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <span>Transportadora 1</span>
+                                <span>R$ 20,00</span>
+                            </div>
+                        }
+                        sx={{
+                            '.css-rizt0-MuiTypography-root': {
+                            width: "100%"
+                            }
+                        }}
+                    />
+                    <p style={{marginBottom: "5px"}}>Chegará até: {new Date(new Date().setDate(new Date().getDate() + 5)).toLocaleDateString()}</p>
+                    <FormControlLabel
+                        value="transportadora2"
+                        control={<Radio />}
+                        label={
+                            <div style={{ display: 'flex', justifyContent: 'space-between', width: "100%" }}>
+                                <span>Transportadora 2</span>
+                                <span>R$ 25,00</span>
+                            </div>
+                        }
+                        sx={{
+                            '.css-rizt0-MuiTypography-root': {
+                            width: "100%"
+                            }
+                        }}
+                    />
+                    <p style={{marginBottom: "5px"}}>Chegará até: {new Date(new Date().setDate(new Date().getDate() + 3)).toLocaleDateString()}</p>
+                    <FormControlLabel
+                        value="transportadora3"
+                        control={<Radio />}
+                        label={
+                            <div style={{ display: 'flex', justifyContent: 'space-between', width: "100%" }}>
+                                <span>Transportadora 3</span>
+                                <span>R$ 30,00</span>
+                            </div>
+                        }
+                        sx={{
+                            '.css-rizt0-MuiTypography-root': {
+                            width: "100%"
+                            }
+                        }}
+                    />
+                    <p>Chegará até: {new Date(new Date().setDate(new Date().getDate() + 2)).toLocaleDateString()}</p>
+                </div>
+            </RadioGroup>
+        </FormControl>
+    );
+}
