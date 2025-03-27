0226b560d9e70e2e17b7cd4a810aff6d342bd363
Kaique-Parente <kaique.parente.game@outlook.com>
2025-03-27 12:51:04 -0300

    [FRONTEND ADD] Carrinho Context com Local Storage

--- frontend/context/CarrinhoContext.js
+++ frontend/context/CarrinhoContext.js
@@ -1,28 +1,49 @@
 "use client"
 
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) => {
     const [carrinho, setCarrinho] = useState([]);
 
+    const salvarCarrinhoNoLocalStorage = (novoCarrinho) => {
+        localStorage.setItem("carrinho", JSON.stringify(novoCarrinho));
+    };
+
+    // Carregar o carrinho do localStorage, se existir
+    useEffect(() => {
+        const carrinhoSalvo = localStorage.getItem("carrinho");
+        if (carrinhoSalvo) {
+            setCarrinho(JSON.parse(carrinhoSalvo));
+        }
+    }, []);
+
+    // Função para adicionar item ao carrinho
     const adicionarItem = (novoItem) => {
         setCarrinho((prev) => {
-            const itemExistente = prev.find((item) => item.produtoId === novoItem.produtoId);
-            const itemTamanhoExistente = prev.find((item) => item.produtoTamanho === novoItem.produtoTamanho)
+            const itemExistente = prev.find(
+                (item) => item.produtoId === novoItem.produtoId && item.produtoTamanho === novoItem.produtoTamanho
+            );
 
-            if (itemExistente && itemTamanhoExistente) {
-                return prev.map((item) =>
-                    item.produtoId === novoItem.produtoId
+            let novoCarrinho;
+            if (itemExistente) {
+                novoCarrinho = prev.map((item) =>
+                    item.produtoId === novoItem.produtoId && item.produtoTamanho === novoItem.produtoTamanho
                         ? { ...item, quantidade: item.quantidade + novoItem.quantidade }
                         : item
                 );
             } else {
-                return [...prev, novoItem];
+                novoCarrinho = [...prev, novoItem];
             }
+
+            // Salva o carrinho atualizado no localStorage
+            salvarCarrinhoNoLocalStorage(novoCarrinho);
+            return novoCarrinho;
         });
     };
+
+    const quantidadeTotal = carrinho.reduce((total, item) => total + item.quantidade, 0);
 
     return (
         <CarrinhoContext.Provider value={{ carrinho, adicionarItem }}>
