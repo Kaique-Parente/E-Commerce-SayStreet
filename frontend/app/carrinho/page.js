d860e7a5bb5849f06f1e06c1b496ac015324f37e
Kaique-Parente <kaique.parente.game@outlook.com>
2025-03-27 18:36:44 -0300

    [FRONTEND ADD] Identificador no Carrinho.

--- frontend/app/carrinho/page.js
+++ frontend/app/carrinho/page.js
@@ -23,6 +23,100 @@ const Container = styled.div`
     }
 `;
 
+const IdentificadorContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #D9D9D9;
+    width: 100%;
+    margin: 30px 0px;
+    position: relative;
+
+    &:not(:last-child)::after {
+        content: "";
+        position: absolute;
+        right: -30px;
+        width: 0px;
+        height: 100%;
+        background: #D9D9D9;
+        clip-path: polygon(0 0, 100% 50%, 0 100%);
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+        gap: 15px;
+        position: relative;
+        width: 100%;
+    }
+
+    .identificador {
+        background-color: #042940;
+        color: #CFCFCF;
+        border-radius: 50%;
+        padding: 5px 10px;
+        margin: 20px;
+        font-size: 16px;
+    }
+
+    img {
+        position: absolute;
+    }
+
+    .seta-identificador1 {
+        left: 84%;
+    }
+
+    .seta-identificador2 {
+        right: -9%;
+    }
+`;
+
+const Etapa = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: center;
+    padding: 10px;
+
+    position: relative;
+
+    &.ativo {
+        background-color: #005C53;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 6px;
+        position: relative;
+        clip-path: polygon(90% 0, 100% 50%, 90% 100%, 0% 100%, 0 50%, 0% 0%);
+    }
+
+    &.ativo-2 {
+        background-color: #005C53;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 6px;
+        position: relative;
+        clip-path: polygon(90% 0%, 100% 50%, 90% 100%, 0% 100%, 10% 50%, 0% 0%);
+    }
+
+    &.ativo-3 {
+        background-color: #005C53;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 6px;
+        position: relative;
+        clip-path: polygon(90% 0%, 100% 0%, 100% 100%, 0% 100%, 10% 50%, 0% 0%);
+    }
+
+    .identificador {
+        background-color: ${(props) => (props.ativo ? "white" : "#042940")};
+        color: ${(props) => (props.ativo ? "#042940" : "#CFCFCF")};
+        border-radius: 50%;
+        padding: 5px 10px;
+    }
+`;
+
+
 const ContainerCards = styled.div`
     display: flex;
     flex-direction: column;
@@ -194,7 +288,7 @@ const DetailsContainer = styled.div`
 
                 font-size: 14px;
             }
-            
+
             button{
                 width: 25%;
                 background-color: rgb(8, 61, 94);
@@ -221,133 +315,139 @@ export default function Carrinho() {
             <NavBar />
             <Container>
                 <div>
-                    <div>
-                        <div>1</div>
-                        <span>Carrinho</span>
-                    </div>
-                    <div>
-                        <div>2</div>
-                        <span>Identificação</span>
-                    </div>
-                    <div>
-                        <div>3</div>
-                        <span>Pagamento</span>
-                    </div>
-                </div>
+                    <IdentificadorContainer>
+                        <Etapa className="ativo">
+                            <div className="identificador">1</div>
+                            <span>Carrinho</span>
+                            <Image className="seta-identificador1" width={90} height={90} src={'/web/seta-identificador.svg'} alt="Imagem de uma seta"/>
+                        </Etapa>
 
-                <div style={{ display: "flex", gap: "30px" }}>
-                    <ContainerCards>
-                        {carrinho.length === 0 ? (
-                            <EmptyCartMessage>
-                                Não existe nenhum produto no carrinho.
-                            </EmptyCartMessage>
-                        ) : (
-                            carrinho.map((item) => {
-                                return (
-                                    <div key={`${item.produtoId}-${item.produtoTamanho}`}>
-                                        <CardProduct>
-                                            <div>
-                                                {item.imagem && item.imagem.url ? (
-                                                    <Image
-                                                        width={150}
-                                                        height={150}
-                                                        src={item.imagem.url}
-                                                        alt={item.produtoNome}
-                                                        style={{ objectFit: "contain", background: "white" }}
-                                                    />
-                                                ) : (
-                                                    <p>Imagem não disponível</p>
-                                                )}
-                                            </div>
+                        <Etapa className="">
+                            <div className="identificador">2</div>
+                            <span>Identificação</span>
+                            <Image className="seta-identificador2" width={90} height={90} src={'/web/seta-identificador.svg'} alt="Imagem de uma seta"/>
+                        </Etapa>
 
-                                            <div className="nome-produto">
-                                                <Link href={""}>{item.produtoNome}</Link>
-                                                <p>Tamanho: {item.produtoTamanho}</p>
-                                            </div>
+                        <Etapa className="">
+                            <div className="identificador">3</div>
+                            <span>Pagamento</span>
+                        </Etapa>
+                    </IdentificadorContainer>
 
-                                            <div className="quantidade">
-                                                <h3>Quantidade</h3>
-                                                <div className="seta-container">
-                                                    <button className="seta seta-esquerda"
-                                                        onClick={() => decrementarQuantidade(item.produtoId, item.produtoTamanho)}
-                                                        disabled={item.quantidade <= 1} // Desabilita a seta se a quantidade for 1
-                                                    >
+                    <div style={{ display: "flex", gap: "30px" }}>
+                        <ContainerCards>
+                            {carrinho.length === 0 ? (
+                                <EmptyCartMessage>
+                                    Não existe nenhum produto no carrinho.
+                                </EmptyCartMessage>
+                            ) : (
+                                carrinho.map((item) => {
+                                    return (
+                                        <div key={`${item.produtoId}-${item.produtoTamanho}`}>
+                                            <CardProduct>
+                                                <div>
+                                                    {item.imagem && item.imagem.url ? (
                                                         <Image
-                                                            width={18}
-                                                            height={18}
-                                                            src={"/web/seta-esquerda.svg"}
-                                                            alt="Ícone de seta para a esquerda"
+                                                            width={150}
+                                                            height={150}
+                                                            src={item.imagem.url}
+                                                            alt={item.produtoNome}
+                                                            style={{ objectFit: "contain", background: "white" }}
                                                         />
-                                                    </button>
-                                                    <p>{item.quantidade}</p>
-                                                    <button className="seta seta-direita"
-                                                        onClick={() => incrementarQuantidade(item.produtoId, item.produtoTamanho)}
-                                                    >
-                                                        <Image
-                                                            width={18}
-                                                            height={18}
-                                                            src={"/web/seta-direita.svg"}
-                                                            alt="Ícone de seta para a direita"
-                                                        />
-                                                    </button>
-                                                </div>
-                                                <div>
-                                                    <button className="btn-remover"
-                                                        onClick={() => removerProduto(item.produtoId, item.produtoTamanho)}>
-                                                        <Image width={16} height={16} src={"/backoffice/lixo.svg"} alt="Ícone de Lixo" />
-                                                        <span>Remover</span>
-                                                    </button>
-                                                </div>
-                                            </div>
-
-                                            <div className="valores">
-                                                <div>
-                                                    <h3>Valor unitário</h3>
-                                                    <p>R$ {item.produtoPreco}</p>
+                                                    ) : (
+                                                        <p>Imagem não disponível</p>
+                                                    )}
                                                 </div>
 
-                                                <div>
-                                                    <h3>Valor total</h3>
-                                                    <p>R$ {(item.produtoPreco * item.quantidade).toFixed(2)}</p>
+                                                <div className="nome-produto">
+                                                    <Link href={""}>{item.produtoNome}</Link>
+                                                    <p>Tamanho: {item.produtoTamanho}</p>
                                                 </div>
-                                            </div>
-                                        </CardProduct>
-                                    </div>
-                                );
-                            })
-                        )}
-                    </ContainerCards>
 
-                    <DetailsContainer>
-                        <div className="resumo-pedido">
-                            <h2>Resumo do Pedido</h2>
-                            <p>Valor dos produtos: <span>R$ {0.00}</span></p>
-                            <p>Frete: <span>R$ {0.00}</span></p>
-                            <div className="total-pedido">
-                                <h3>Valor Total: <span>R$ {0.00}</span></h3>
-                                <p>(em até <span>10x</span> de <span>R$ 585,88</span> sem juros)</p>
+                                                <div className="quantidade">
+                                                    <h3>Quantidade</h3>
+                                                    <div className="seta-container">
+                                                        <button className="seta seta-esquerda"
+                                                            onClick={() => decrementarQuantidade(item.produtoId, item.produtoTamanho)}
+                                                            disabled={item.quantidade <= 1} // Desabilita a seta se a quantidade for 1
+                                                        >
+                                                            <Image
+                                                                width={18}
+                                                                height={18}
+                                                                src={"/web/seta-esquerda.svg"}
+                                                                alt="Ícone de seta para a esquerda"
+                                                            />
+                                                        </button>
+                                                        <p>{item.quantidade}</p>
+                                                        <button className="seta seta-direita"
+                                                            onClick={() => incrementarQuantidade(item.produtoId, item.produtoTamanho)}
+                                                        >
+                                                            <Image
+                                                                width={18}
+                                                                height={18}
+                                                                src={"/web/seta-direita.svg"}
+                                                                alt="Ícone de seta para a direita"
+                                                            />
+                                                        </button>
+                                                    </div>
+                                                    <div>
+                                                        <button className="btn-remover"
+                                                            onClick={() => removerProduto(item.produtoId, item.produtoTamanho)}>
+                                                            <Image width={16} height={16} src={"/backoffice/lixo.svg"} alt="Ícone de Lixo" />
+                                                            <span>Remover</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                                <div className="valores">
+                                                    <div>
+                                                        <h3>Valor unitário</h3>
+                                                        <p>R$ {item.produtoPreco}</p>
+                                                    </div>
+
+                                                    <div>
+                                                        <h3>Valor total</h3>
+                                                        <p>R$ {(item.produtoPreco * item.quantidade).toFixed(2)}</p>
+                                                    </div>
+                                                </div>
+                                            </CardProduct>
+                                        </div>
+                                    );
+                                })
+                            )}
+                        </ContainerCards>
+
+                        <DetailsContainer>
+                            <div className="resumo-pedido">
+                                <h2>Resumo do Pedido</h2>
+                                <p>Valor dos produtos: <span>R$ {0.00}</span></p>
+                                <p>Frete: <span>R$ {0.00}</span></p>
+                                <div className="total-pedido">
+                                    <h3>Valor Total: <span>R$ {0.00}</span></h3>
+                                    <p>(em até <span>10x</span> de <span>R$ 585,88</span> sem juros)</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="frete">
-                            <h2>Calcular Frete</h2>
-                            <div className="input-container">
-                                <input type="number" placeholder="CEP:" />
-                                <button>OK</button>
+                            <div className="frete">
+                                <h2>Calcular Frete</h2>
+                                <div className="input-container">
+                                    <input type="number" placeholder="CEP:" />
+                                    <button>OK</button>
+                                </div>
+                                <TransportadorasGroup transportadora={transportadora} setTransportadora={setTransportadora} />
                             </div>
-                            <TransportadorasGroup transportadora={transportadora} setTransportadora={setTransportadora} />
-                        </div>
 
-                        <div style={{ marginTop: "5px" }}>
-                            <BotaoPersonalizado
-                                width={"100%"}
-                                height={"50px"}
-                                color={"amarelo"}
-                            >
-                                Finalizar a Compra
-                            </BotaoPersonalizado>
-                        </div>
-                    </DetailsContainer>
+                            <div style={{ marginTop: "5px" }}>
+                                <BotaoPersonalizado
+                                    width={"100%"}
+                                    height={"50px"}
+                                    color={"amarelo"}
+                                >
+                                    Finalizar a Compra
+                                </BotaoPersonalizado>
+                            </div>
+                        </DetailsContainer>
+                    </div>
                 </div>
 
                 <Footer />
