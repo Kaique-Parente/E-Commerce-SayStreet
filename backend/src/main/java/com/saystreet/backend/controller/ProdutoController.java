b7274164014c40b5d1f30567c640ab05b2542007
RodrigoPereir4 <digolima167@gmail.com>
2025-04-06 01:06:21 -0300

    [BACKEND REFACTOR] Refatoração no tratamento de exceções nos métodos de criar e editar produto.

--- backend/src/main/java/com/saystreet/backend/controller/ProdutoController.java
+++ backend/src/main/java/com/saystreet/backend/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.saystreet.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,8 @@ import com.saystreet.backend.dto.ProdutoDto;
 import com.saystreet.backend.models.ProdutoModel;
 import com.saystreet.backend.service.ProdutoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/produto")
 @CrossOrigin(origins = "*")
@@ -26,39 +29,49 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
+    @Operation(summary = "Cadastrar produto", description = "Método para registrar um produto.", tags = "Produto")
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> salvar(@RequestBody ProdutoDto produto){
-        return ResponseEntity.ok(produtoService.create(produto));
+    public ResponseEntity<?> salvar(@RequestBody ProdutoDto produto) {
+        try {
+            return ResponseEntity.ok(produtoService.create(produto));
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Erro: Campos obrigatórios não foram preenchidos corretamente.");
+        }
     }
-    
-     @GetMapping("/buscar/{id}")
+
+    @Operation(summary = "Buscar produto", description = "Método para buscar um produto pelo ID.", tags = "Produto")
+    @GetMapping("/buscar/{id}")
     public ResponseEntity<ProdutoModel> buscarProduto(@PathVariable Long id) {
         ProdutoModel produto = produtoService.buscarProdutoPorId(id);
         return ResponseEntity.ok(produto);
     }
 
+    @Operation(summary = "Editar produto", description = "Método para editar um produto, buscando pelo ID.", tags = "Produto")
     @PutMapping("/editar/{id}")
     public ResponseEntity<String> editarProduto(@PathVariable Long id,
             @RequestBody ProdutoDto produtoDto) {
 
         return ResponseEntity.ok(produtoService.editarProduto(id, produtoDto));
     }
-    
+
+    @Operation(summary = "Alterar Status do produto", description = "Método para invativar/reativar um produto.", tags = "Produto")
     @PutMapping("/alterar-status/{id}")
     public ResponseEntity<String> alterarStatus(@PathVariable Long id, @RequestParam boolean status) {
         return ResponseEntity.ok(produtoService.alterarStatus(id, status));
     }
 
+    @Operation(summary = "Alterar quantidade", description = "Método para alterar a quantidade de um produto no estoque.", tags = "Produto")
     @PutMapping("/alterar-qtd/{id}")
-    public ResponseEntity<?> alterarQtd(@PathVariable Long id, @RequestBody ProdutoDto produtoDto){
+    public ResponseEntity<?> alterarQtd(@PathVariable Long id, @RequestBody ProdutoDto produtoDto) {
         return ResponseEntity.ok(produtoService.editarQTD(id, produtoDto));
     }
 
+    @Operation(summary = "Listar produtos", description = "Método listar todos os produtos.", tags = "Produto")
     @GetMapping("/listar")
     public List<ProdutoModel> listar() {
         return produtoService.listAll();
     }
-    
-
 
 }
