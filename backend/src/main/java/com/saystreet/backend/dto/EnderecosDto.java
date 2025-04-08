f4d5a910e95c619d36ad3bfeba6d84910030d0c7
RodrigoPereir4 <digolima167@gmail.com>
2025-04-07 22:59:08 -0300

    [BACKEND ADD] Classe DTO de endereços.

---
+++ backend/src/main/java/com/saystreet/backend/dto/EnderecosDto.java
@@ -0,0 +1,23 @@
+package com.saystreet.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecosDto {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private Integer numero;
+    private boolean enderecoPadrao;
+}
