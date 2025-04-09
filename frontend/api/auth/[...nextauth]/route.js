9eae5708a2d12f8fdf2e2c9e433263a65225f398
Kaique-Parente <kaique.parente.game@outlook.com>
2025-04-09 16:49:41 -0300

    [FRONTEND ADD] Iniciando o NextAuthJS.

---
+++ frontend/api/auth/[...nextauth]/route.js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Senha", type: "password" },
+      },
+      async authorize(credentials) {
+        const res = await fetch("http://localhost:8080/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) return user;
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
