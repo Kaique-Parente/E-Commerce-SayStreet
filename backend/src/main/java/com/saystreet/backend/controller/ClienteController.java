91b2fcc2422bd2b39a622a5e9546286de88e83bb
RodrigoPereir4 <digolima167@gmail.com>
2025-04-13 19:34:13 -0300

    [BACKEND ADD] Método para editar e listar um cliente na classe controller

--- backend/src/main/java/com/saystreet/backend/controller/ClienteController.java
+++ backend/src/main/java/com/saystreet/backend/controller/ClienteController.java
@@ -1,15 +1,21 @@
 package com.saystreet.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saystreet.backend.dto.ClienteDto;
+import com.saystreet.backend.models.ClienteModel;
 import com.saystreet.backend.service.ClienteService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,14 +24,28 @@ import io.swagger.v3.oas.annotations.Operation;
 @RequestMapping("/cliente")
 @CrossOrigin(origins = "*")
 public class ClienteController {
-    
+
     @Autowired
     ClienteService clienteService;
 
-    @Operation(summary = "Cadastrar Cliente", description = "Método para cadastrar um cliente.",
-    tags = "cliente")
+    @Operation(summary = "Cadastrar Cliente", description = "Método para cadastrar um cliente.", tags = "cliente")
     @PostMapping("/cadastrar")
-    public ResponseEntity<String> cadastrar (@RequestBody ClienteDto clienteDto) throws Exception{
+    public ResponseEntity<String> cadastrar(@RequestBody ClienteDto clienteDto) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.create(clienteDto));
     }
+
+    @Operation(summary = "Editar Cliente", description = "Método para editar um cliente.", tags = "cliente")
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<String> editar(@PathVariable Long id, @RequestBody
+            ClienteDto clienteDto) throws Exception {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.editCliente(id, clienteDto));
+    }
+
+    @Operation(summary = "Listar clientes", description = "Método listar todos os clientes.", tags = "cliente")
+    @GetMapping("/listar")
+    public List<ClienteModel> listar() {
+        return clienteService.listAll();
+    }
+
 }
