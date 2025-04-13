56867b5927908d453bf2ffef2f273bfeeb2c72e0
Kaique-Parente <kaique.parente.game@outlook.com>
2025-04-13 15:34:58 -0300

    [FRONTEND ADD] Sidebar com logout finalizada.

--- frontend/components/CoreUI/Sidebar.jsx
+++ frontend/components/CoreUI/Sidebar.jsx
@@ -8,34 +8,59 @@ import {
   CNavGroup,
   CNavItem,
   CNavTitle,
+  CSidebarToggler,
 } from '@coreui/react'
 
 import CIcon from '@coreui/icons-react'
 import { cilHome, cilCloudDownload, cilLayers, cilPuzzle, cilSpeedometer } from '@coreui/icons'
 import Image from 'next/image'
+import styled from 'styled-components'
+import { signOut } from 'next-auth/react'
+
+const LogoutContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  padding: 16px 0;
+  border-radius: 6px;
+
+  &:hover{
+    background: #f3f4f7;
+  }
+`
 
 export const Sidebar = () => {
   return (
     <>
       <CSidebar style={{ marginTop: "80px", boxShadow: "rgba(40, 41, 61, 0.04) 0px 2px 4px, rgba(96, 97, 112, 0.16) 0px 8px 16px" }} className="border-end" unfoldable size='xl'>
-        <CSidebarHeader className="border-bottom">
-          <CSidebarBrand>SAY</CSidebarBrand>
-        </CSidebarHeader>
         <CSidebarNav>
-          <CNavTitle>Nav Title</CNavTitle>
+          <CNavTitle>Menu de Opções</CNavTitle>
           <CNavItem href="#">
-            <Image style={{marginRight: "15px"}} src={"/web/sidebar/home.png"} width={28} height={28} alt='home' /> Início
+            <Image style={{ marginRight: "15px" }} src={"/web/sidebar/home.png"} width={28} height={28} alt='home' /> Início
           </CNavItem>
           <CNavItem href="#">
-            <Image style={{marginRight: "15px"}} src={"/web/sidebar/pessoa.png"} width={28} height={28} alt='pessoa' /> Meus dados
+            <Image style={{ marginRight: "15px" }} src={"/web/sidebar/pessoa.png"} width={28} height={28} alt='pessoa' /> Meus dados
           </CNavItem>
           <CNavItem href="#">
-            <Image style={{marginRight: "15px"}} src={"/web/sidebar/sacola.png"} width={28} height={28} alt='sacola' /> Meus pedidos
+            <Image style={{ marginRight: "15px" }} src={"/web/sidebar/sacola.png"} width={28} height={28} alt='sacola' /> Meus pedidos
           </CNavItem>
           <CNavItem href="#">
-            <Image style={{marginRight: "15px"}} src={"/web/sidebar/carteira.png"} width={28} height={28} alt='carteira' /> Minha carteira
+            <Image style={{ marginRight: "15px" }} src={"/web/sidebar/carteira.png"} width={28} height={28} alt='carteira' /> Minha carteira
           </CNavItem>
         </CSidebarNav>
+        <CSidebarHeader style={{overflow: "hidden", display: "flex", justifyContent: "space-between", padding: "0px"}} className="border-top">
+          <LogoutContainer onClick={() => signOut({ callbackUrl: "/login" })}>
+            <Image 
+              style={{margin: "0px 20px 0px 15px"}} 
+              src={"/web/sidebar/saida.png"} 
+              width={28} height={28} alt='saida' 
+            />
+            <span>Sair</span>
+          </LogoutContainer>  
+        </CSidebarHeader>
       </CSidebar>
     </>
   )
