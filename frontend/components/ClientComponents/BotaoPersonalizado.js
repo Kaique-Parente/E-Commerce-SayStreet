db9fef5b3d1edf12d69df5c4bb03e922b7290dac
Kaique-Parente <kaique.parente.game@outlook.com>
2025-04-14 13:31:31 -0300

    [FRONTEND UPDATE] Botão personalizado recebendo style por props.

--- frontend/components/ClientComponents/BotaoPersonalizado.js
+++ frontend/components/ClientComponents/BotaoPersonalizado.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const Botao = styled.button`
     width: ${(props) => props.width || '150px'}; 
     height: ${(props) => props.height || '45px'};
-    background-color:${(props) => (props.color === "amarelo" ? "rgba(255, 227, 23, 95)" : props.color === "marrom" ? "#583939" : "#FF2600")};
-    color: ${(props) => (props.color !== "amarelo" ? "#005C53" : props.color === "marrom" ? "#F8AE27" : "#584439")};
+    background-color:${(props) => (props.color === "amarelo" ? "rgba(255, 227, 23, 95)" : props.color === "marrom" ? "#005C53" : "#FF2600")};
+    color: ${(props) => (props.color === "amarelo" ? "#005C53" : props.color === "marrom" ? "#faebd7" : "#584439")};
 
     border-radius: 8px;
     padding: 8px 12px;
@@ -16,12 +16,12 @@ const Botao = styled.button`
     cursor: pointer;
 
     &:hover{
-        background-color:${(props) => (props.color === "amarelo" ? "#B3A010" : props.color === "marrom" ? "#0F3434" : "#911600")};
+        background-color:${(props) => (props.color === "amarelo" ? "#B3A010" : props.color === "marrom" ? "#023933" : "#911600")};
     }
 `
 
 export default function BotaoPersonalizado(props){
     return(
-        <Botao width={props.width} height={props.height} type={props.type} onClick={props.onClick} color={props.color}>{props.children}</Botao>
+        <Botao style={props.style} width={props.width} height={props.height} type={props.type} onClick={props.onClick} color={props.color}>{props.children}</Botao>
     );
 }
