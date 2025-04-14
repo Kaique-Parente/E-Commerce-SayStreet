4c2c80a5f0c5da65f9cf9ae3e94ea377c2471e02
Kaique-Parente <kaique.parente.game@outlook.com>
2025-04-14 01:14:50 -0300

    [FRONTEND UPDATE] Input personalizado recebendo disabled.

--- frontend/components/ClientComponents/InputPersonalizado.js
+++ frontend/components/ClientComponents/InputPersonalizado.js
@@ -11,6 +11,8 @@ const InputStyle = styled.input`
     border-radius: 6px;
 
     font-size: 14px;
+
+    background: ${(props) => props.disabled ? "#716b6b9c" : "none"};
     
     &:focus{
         border: 2px solid rgb(43, 34, 29);
@@ -21,6 +23,7 @@ export default function InputPersonalizado(props){
     return(
         <InputStyle 
             required={props.isRequired || false}
+            disabled={props.disabled}
             id={props.id} 
             value={props.value || ""}
             onChange={props.onChange}
