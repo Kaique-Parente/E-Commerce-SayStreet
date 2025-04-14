44f88d786b9f0f1b9c66bcd43b3c7323dd7bab6c
Kaique-Parente <kaique.parente.game@outlook.com>
2025-04-14 16:19:26 -0300

    [FRONTEND ADD] Regex functions.

---
+++ frontend/utils/regex.js
@@ -0,0 +1,25 @@
+export const formatCpf = (value) => {
+    const onlyNumbers = value.replace(/\D/g, '').slice(0, 11); // Máximo 11 dígitos
+
+    return onlyNumbers
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+};
+
+export const validateCpf = (value) => {
+    const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    return cpfRegex.test(value);
+};
+
+export const formatCep = (value) => {
+    const onlyNumbers = value.replace(/\D/g, '').slice(0, 8);
+
+    return onlyNumbers.replace(/^(\d{5})(\d{1,3})$/, '$1-$2');
+};
+
+export const validateCep = (value) => {
+    const cepRegex = /^\d{5}-\d{3}$/;
+    return cepRegex.test(value);
+};
+
